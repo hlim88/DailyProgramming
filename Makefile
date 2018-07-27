26cfd5caad64581145efa7fde17e1d527df9a2d2
Hyun Lim <hylim1988@gmail.com>
2018-07-27 17:25:40 -0600

    Add week 9 solution

--- Makefile
+++ Makefile
@@ -1,14 +1,16 @@
 CC = g++
 
-C_FLAGS = -Wall -std=c++11
+C_FLAGS = -Wall -std=c++11 
 
-SRC1 = week2.cpp 
-SRC2 = week4.cpp
-#SRC3 = week5.cpp
+SRC2 = week2.cpp 
+SRC4 = week4.cpp
+SRC7 = week7.cpp
+SRC9 = week9.cpp
 
 all: 
-	$(CC) $(SRC1) $(C_FLAGS) -o week2
-	$(CC) $(SRC2) $(C_FLAGS) -o week4
-	#$(CC) $(SRC3) $(C_FLAGS) -o week5
+	$(CC) $(SRC2) $(C_FLAGS) -o week2
+	$(CC) $(SRC4) $(C_FLAGS) -o week4
+	$(CC) $(SRC7) $(C_FLAGS) -o week7
+	$(CC) $(SRC9) $(C_FLAGS) -o week9
 clean:
-	rm -rf *.o week2 week4 week5
+	rm -rf *.o week2 week4 week7 week9
--- week9.cpp
+++ week9.cpp
@@ -10,4 +10,59 @@ Output: [5, 3, -1, 0, 0]
 Input: [3, 0, 3]
 Output: [3, 3, 0]
 */
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <cstdlib>
+#include <cstdio>
+
+using namespace std;
+
+// Define display function
+
+void display(int *arr, int len){
+	
+	for (int *tmp = arr; tmp < arr+len; tmp++)
+		cout<<" "<<*(tmp);
+ 	        cout<<"\n";
+
+}
+
+int main(void) {
+	//Getting input
+	int len=0;
+	cout<<"Please enter your size of array : ";
+	cin>>len;
+
+	int arr_temp[len];
+     	cout<<"Please enter your elements of array : ";
+  	  for (int i = 0 ; i < len; i++){
+		scanf("%d", &arr_temp[i]);
+	  }
+
+	cout<<"This is your input : \n";
+	display(arr_temp, len);
+
+	//Move all zeros in array to right
+	int arr[len];
+ 	
+	int j = 0;
+	int k = len-1;
+
+	for (int i = 0; i < len; i++){
+	  if(arr_temp[i] != 0){
+	     arr[j] = arr_temp[i];
+	     j++;
+	  } else {
+	     arr[k] = 0;
+	     k--;
+	  }
+	}
+
+	cout<<"Thit is the result ! \n";
+	display(arr, len);
+
+	return 0;
+}
+
 
