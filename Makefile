6dd3322f42b9eaabd5b809c43879e1fc2d0808ce
Hyun Lim <hylim1988@gmail.com>
2018-08-15 11:02:13 -0600

    Week8 solved

--- Makefile
+++ Makefile
@@ -5,6 +5,7 @@ C_FLAGS = -Wall -std=c++11
 SRC2 = week2.cpp 
 SRC4 = week4.cpp
 SRC7 = week7.cpp
+SRC8 = week8.cpp
 SRC9 = week9.cpp
 SRC12 = week12.cpp
 
@@ -12,7 +13,8 @@ all:
 	$(CC) $(SRC2) $(C_FLAGS) -o week2
 	$(CC) $(SRC4) $(C_FLAGS) -o week4
 	$(CC) $(SRC7) $(C_FLAGS) -o week7
+	$(CC) $(SRC8) $(C_FLAGS) -o week8
 	$(CC) $(SRC9) $(C_FLAGS) -o week9
 	$(CC) $(SRC12) $(C_FLAGS) -o week12
 clean:
-	rm -rf *.o week2 week4 week7 week9 week12
+	rm -rf *.o week2 week4 week7 week8 week9 week12
--- week8.cpp
+++ week8.cpp
@@ -13,6 +13,88 @@ Output: Does not exist.
 #include <iostream>
 #include <cstdlib>
 #include <cmath>
+#include <cstdlib>
+#include <cstdio>
 
 using namespace std;
 
+//Define display function
+void display(int *arr, int len){
+
+        for (int *tmp = arr; tmp < arr+len; tmp++)
+                cout<<" "<<*(tmp);
+                cout<<"\n";
+
+}
+//Define finding minimum elements of an array
+int find_min(int arr[], int len){
+
+	int i = arr[0];
+
+	for (int j = 0; j < len; j++) {
+	  if(i > arr[j])
+	    i = arr[j];
+	}
+	return i;
+}
+//Define finding max elements of an array
+int find_max(int arr[], int len){
+
+	int i = arr[0];
+
+	for (int j = 0; j < len; j++) {
+	  if(i < arr[j])
+	    i = arr[j];
+	}
+	return i;
+}
+
+int main(void) {
+
+        int len=0;
+        cout<<"Please enter your size of array : ";
+        cin>>len;
+
+        int arr_temp[len];
+	if (len < 2) {
+	  cout<<"Please enter array size larger than or eqaul to 2\n";
+	  return 0;
+	} else {
+          cout<<"Please enter your elements of array : ";
+            for (int i = 0 ; i < len; i++){
+                  scanf("%d", &arr_temp[i]);
+            }
+
+          cout<<"This is your input : \n";
+          display(arr_temp, len);
+        }
+
+	//Finding min val for crieteria
+	int minVal = find_min(arr_temp,len);
+
+	//Define intermediate vars
+	int temp1 = arr_temp[0]; //This will save largest
+	int temp2 = minVal; //This will save second largest
+
+	for (int i = 0; i < len; i++) {
+	  if (arr_temp[i] > temp1){
+	    temp2 = temp1;
+	    temp1 = arr_temp[i];
+  	  } else if (arr_temp[i] > temp2 && arr_temp[i] != temp1){
+	    temp2 = arr_temp[i];		
+	  }   
+	}
+
+
+	//Compute max value for checking	   
+ 	int maxVal = find_max(arr_temp,len);
+
+	if (temp2 == minVal && temp2 == maxVal) {
+	  cout << "There is no second largest value\n";
+	} else {
+	  cout<<"This is the answer : "<<temp2;
+	  cout<<"\n";
+	}
+	
+	return 0;
+}
