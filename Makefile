a1ac4de5ff91c663586c54b9bf28a7005320b825
Hyun Lim <hylim1988@gmail.com>
2018-08-17 10:08:16 -0600

    Add week1 solution

--- Makefile
+++ Makefile
@@ -2,14 +2,17 @@ CC = g++
 
 C_FLAGS = -Wall -std=c++11 
 
+SRC1 = week1.cpp 
 SRC2 = week2.cpp 
 SRC4 = week4.cpp
+SRC5 = week5.cpp
 SRC7 = week7.cpp
 SRC8 = week8.cpp
 SRC9 = week9.cpp
 SRC12 = week12.cpp
 
 all: 
+	$(CC) $(SRC1) $(C_FLAGS) -o week1
 	$(CC) $(SRC2) $(C_FLAGS) -o week2
 	$(CC) $(SRC4) $(C_FLAGS) -o week4
 	$(CC) $(SRC7) $(C_FLAGS) -o week7
@@ -17,4 +20,4 @@ all:
 	$(CC) $(SRC9) $(C_FLAGS) -o week9
 	$(CC) $(SRC12) $(C_FLAGS) -o week12
 clean:
-	rm -rf *.o week2 week4 week7 week8 week9 week12
+	rm -rf *.o week1 week2 week4 week7 week8 week9 week12
--- week1.cpp
+++ week1.cpp
@@ -14,3 +14,54 @@ Input: [2, 4, -2, -3, 8]
 Output: 9 // 2 + 4 + (-2) + (-3) + 8
 
 */
+
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <cstdlib>
+#include <cstdio>
+
+using namespace std;
+
+// Define display function
+
+void display(int *arr, int len){
+
+        for (int *tmp = arr; tmp < arr+len; tmp++)
+                cout<<" "<<*(tmp);
+                cout<<"\n";
+
+}
+
+int main(void) {
+
+        int len=0;
+        cout<<"Please enter your size of array : ";
+        cin>>len;
+
+        int arr_temp[len];
+        cout<<"Please enter your elements of array : ";
+          for (int i = 0 ; i < len; i++){
+                scanf("%d", &arr_temp[i]);
+          }
+
+        cout<<"This is your input : \n";
+        display(arr_temp, len);
+
+
+	//Compare current sum and max sum
+	int currSum = arr_temp[0];
+	int maxSum = arr_temp[0]; 
+
+	for (int i = 1; i < len; i++) {
+	
+	   currSum = max(currSum + arr_temp[i], arr_temp[i]);
+	   maxSum = max(currSum, maxSum);
+	  
+	}
+
+	//Print solution
+	cout<<"This is the answer : "<<maxSum<<"\n";
+
+	return 0;
+}
